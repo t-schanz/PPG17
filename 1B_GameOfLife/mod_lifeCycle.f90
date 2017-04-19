0ee10afa1da849df34e15cb460ea855102b8dd95
Dr. Suess <heutekeinnachtisch@gmail.com>
2017-04-19 21:28:50 +0200

    Update mod_lifeCycle.f90

--- 1B_GameOfLife/mod_lifeCycle.f90
+++ 1B_GameOfLife/mod_lifeCycle.f90
@@ -1,64 +1,96 @@
-MODULE mod_lifecycle
 
-	! Subroutine zum Zählen der Nachbarn einer Zelle ausgelagert
-	USE mod_cellNeighbours
-	
-	IMPLICIT NONE
-	PRIVATE :: countNeighbours
-	PRIVATE :: cellNeighbours
-	PUBLIC :: developLife
-
-	CONTAINS
-
-	! Erstellt ein Skalarfeld mit den Anzahlen der Nachbarn
-	!  jeder Zelle des Spielfeld
-	! field: Spielfeld, wird nicht modifiziert
-	! neighbourfield: Skalarfeld 
+module mod_lifecycle
 	!
-	SUBROUTINE countNeighbours (field, neighbourField)
-		IMPLICIT NONE
+	implicit none
+	private :: countNeighbours !(matrix)
+	public :: developLife !(matrix, posX, posY, neighbors)
+	
+contains
+
+	! Entwickelt das Spielfeld anhand der folgenden Regeln: (dabei steht lebend für "true" in der Matrix)
+	! - Lebende Zellen mit weniger als zwei Nachbarn sterben.
+	! - Lebende Zellen mit mehr als drei Nachbarn sterben.
+	! - Lebende Zellen mit zwei oder drei Nachbarn überleben.
+	! - Tote Zellen mit genau drei Nachbarn werden belebt.
+	!
+	subroutine developeLife(matrix)
 		!
-		LOGICAL, DIMENSION(30, 20) :: field
-		INTEGER, DIMENSION(30, 20) :: neighbourField
-		! Schleifenvariablen
-		INTEGER :: i, j
-		! Hilfsvariable
-		INTEGER :: n
+		! Spielfeld
+		logical, dimension(:,:), intent(inout) :: matrix
+		! Interne Kopie des Spielfelds
+		logical, dimension(:,:), pointer :: matrixCopy
+		! Dimensionen des Spielfelds
+		integer :: width, height
+		! Schleifenvariablen und Hilfsvariable
+		integer :: i, j, neighbours
 
-		DO i = 1, 30
-			DO j = 1, 20
-				n = 0
-				CALL cellNeighbours(field, i, j, n)
-				neighbourField(i,j) = n
-			END DO
-		END DO
+		width = ubound(matrix, 1)
+		height = ubound(matrix, 2)
+		allocate(matrixCopy(1:width,1:height))
+		matrixCopy = matrix
 
-	END SUBROUTINE countNeighbours
+		do i = 1,width
+			do j = 1,height
+				call countNeighbors(matrixCopy, i, j, neighbours)
+				! Eigentlich Anwendung der Regeln aufs Spielfeld
+				if (matrixCopy(i, j) .eqv. .true.) then
+					matrix(i,j) = ((neighbours > 1) .and. (neighbours < 4))
+				else
+					matrix(i,j) = (neighbours == 3)
+				end if
 
-	! Wendet die Regeln von Convays Game of Life 
-	!  auf das Spielfeld an.
-	SUBROUTINE developLife (field)
-		IMPLICIT NONE
+			end do 
+		end do
+	end subroutine
+
+
+	! Zählt lebende Nachbarn einer Zelle
+	!	matrix: Spielfeld
+	!	posX, posY: Koordinaten der Zelle im Spielfeld
+	!	neighbours: Anzahl der lebendigen Nachbarn einer Zelle
+	!
+	subroutine countNeighbors(matrix, posX, posY, neighbors)
 		!
-		LOGICAL, DIMENSION(30, 20) :: field
-		INTEGER, DIMENSION(30, 20) :: neighbourField
+		! Spielfeld
+		logical, dimension(:,:), intent(in) :: matrix
+		! Koordinaten
+		integer, intent(in) :: posX, posY
+		! (Rückgabewert) Anzahl der lebendigen Nachbarn einer Zelle
+		integer, intent(out) :: neighbors
+		! Dimensionen des Spielfelds
+		integer :: width, height
+		! Grenzen innerhalb derer Nachbarn gezählt werden
+		integer :: left, right, top, bottom
 		! Schleifenvariablen
-		INTEGER :: i, j
+		integer :: i, j
 
-		! Erstellen des Skalarfelds mit Nachbarschaftszahlen
-		CALL countNeighbours(field, neighbourField)
+		neighbors = 0
+		width = ubound(matrix, 1)
+		height = ubound(matrix, 2)
+		! Initialisierung der Grenzen mit Abstand 1 zu den Koordinaten
+		left = posX-1
+		right = posX+1
+		top = posY-1
+		bottom = posY+1
+		! Anpassung der Boundarys für Felder am Spielfeldrand
+		if(posX == 1) left = posX
+		if(posX == width) right = posX
+		if(posY == 1) top = posY
+		if(posY == height) bottom = posY
 
-		! Anwenden der Regeln für die neue Runde
-		DO i = 1, 30
-			DO j = 1, 20
-				IF (field(i, j) .eqv. .TRUE.) THEN
-					field(i,j) = ((neighbourField(i,j) > 1) .AND. (neighbourField(i,j) < 4))
-				ELSE
-					field(i,j) = (neighbourField(i,j) == 3)
-				END IF
-			END DO
-		END DO
+		! Zählen der lebenden Nachbarn in einem 3x3-Kasten um die Koordinate 
+		do i = left,right
+			do j = top,bottom
+				if(matrix(i,j) .eqv. .true.) then       
+					neighbors = neighbors + 1
+				end if
+			end do
+		end do
 
-	END SUBROUTINE
+		! Korrektur um den Wert der Zelle selbst
+		if (matrix(posX, posY) .eqv. .true.) then
+					neighbors = neighbors -1 
+		end if
 
-END MODULE mod_lifecycle
+	end subroutine
+end module
