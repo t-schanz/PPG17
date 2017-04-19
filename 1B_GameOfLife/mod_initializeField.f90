dd9d7b25e2e28ec246aecb99b597bc2267f4537d
Dr. Suess <heutekeinnachtisch@gmail.com>
2017-04-19 21:26:10 +0200

    Übernommen von Andreas
    https://github.com/Enyaar/PP17/

--- 1B_GameOfLife/mod_initializeField.f90
+++ 1B_GameOfLife/mod_initializeField.f90
@@ -1,72 +1,98 @@
-MODULE mod_initializeField
-	IMPLICIT NONE
-	
-	CONTAINS
+module mod_initializeField
+	use mo_utilities
+	implicit none
 
-	SUBROUTINE createField (spielFeld)
-		IMPLICIT NONE
-		
-		LOGICAL, DIMENSION(30,20) :: spielFeld
-		! Schleifenvariablen
-		INTEGER :: i,j
+	! Einbinden des Zeichensatzes UTF-32
+	! (notwendig für die Methode printTwoDLogical)
+	private utf32
+	integer, parameter :: utf32=selected_char_kind('ISO_10646')
 
-		DO i = 1, 30
-			DO j = 1, 20
-				spielFeld(i,j) = .FALSE.
-			END DO
-		END DO
-
-	END SUBROUTINE createField
+contains
+	! subroutine createField(matrix)
+	! subroutine createFigures(matrix)
+	! subroutine printTwoDLogical(outputUnit, matrix)
 	
 
-	SUBROUTINE createFigures (spielFeld, xPosition, yPosition, pattern)
-		IMPLICIT NONE
+	! Erstellt Spielfeld mit Dimensionen 30x20
+	subroutine createField(matrix)
+		!Spielfeld
+		logical, dimension(:,:), pointer, intent(inout) :: matrix
 
-		LOGICAL, DIMENSION(30,20) :: spielFeld
-		! Koordinaten
-		INTEGER :: xPosition, yPosition
-		! Nummer des Musters
-		! Default ist Blinker
-! Eigentlich wären Namen schöner, habe ich irgendwie nicht gebacken bekommen.
-		INTEGER :: pattern
+		!Zuweisen der Matrix
+		allocate(matrix(1:30,1:20))
+		!Belegen mit "false"
+		matrix = .false.
+	end subroutine
+
+	! Setzt die drei Figuren "blinker", "toad" und "beacon" auf das Spielfeld
+	subroutine createFigures(matrix)
+		! Spielfeld
+		logical, dimension(:,:), pointer, intent(inout) :: matrix
+		! Figuren
+		logical, dimension(:,:), pointer :: blinker, toad, beacon
+		! Koordinaten an denen die erste Figur eingesetzt wird. 
+		! Alle anderen folgen in festgesetztem Abstand.
+		integer :: positionX=4, positionY=3
+
+		! Zuweisen und Erstellen der Figur blinker
+		allocate(blinker(1:3,1:1))
+		blinker = .true.
+
+		! Zuweisen und Erstellen der Figur toad
+		allocate(toad(1:4,1:2))
+		toad = .true.
+		toad(1,1) = .false.
+		toad(4,2) = .false.
+
+		! Zuweisen und Erstellen der Figur beacon
+		allocate(beacon(1:6,1:6))
+		beacon = .false.
+		beacon(2:3,2) = .true.
+		beacon(2,3) = .true.
+		beacon(5,4) = .true.
+		beacon(4:5,5) = .true.
+
+		!Setzt Figuren auf die Matrix
+		matrix(positionX:,positionY:) = blinker
+		matrix(positionX+10:,positionY+5:) = toad
+		matrix(positionX+15:,positionY+10:) = beacon
+	end subroutine 
+
+	
+	! Ausgabe auf die Konsole
+	! (Übernommen aus dem Programm Glider)
+	subroutine printTwoDLogical(outputUnit, matrix)
+		integer, intent(in) :: outputUnit
+		logical, dimension(:,:), intent(in) :: matrix
+		integer :: width, height
+		character(kind=utf32, len=1), dimension(:,:), allocatable :: characterMatrix
+		! z'1b' hexadezimale Zahl, dezimaler Wert ist 27 
+		! char(int(z'00B7'), utf32) ist das Zeichen mit der hexad. Nummer 00B7 
+		! des Zeichensatzes 'ISO_10646'
+		! 00B7 = middle dot, 2588 = full block
+		character(kind=utf32, len=1) :: blockChar = char(int(z'2588'), utf32), emptyChar = char(int(z'00B7'), utf32)
+		character(len=22) :: formatString
 		
+		!Create the character matrix.
+		width = ubound(matrix, 1)
+		height = ubound(matrix, 2)
+		allocate(characterMatrix(1:width, 1:height))
+		where (matrix)
+			characterMatrix = blockChar
+		else where
+			characterMatrix = emptyChar
+		end where
+
+		!Write it out.
+		formatString = '(' // intToStr(width) // 'a)'
+		write(outputUnit, formatString) characterMatrix
+		formatString = '(' // intToStr(width) // '("="))'
+		write(outputUnit, formatString)
 		
-		SELECT CASE (pattern)
-			!CASE ("blinker")
-			CASE (1)
-				spielFeld(xPosition, yPosition) = .TRUE.
-				spielFeld(xPosition + 1, yPosition) = .TRUE.
-				spielFeld(xPosition + 2, yPosition) = .TRUE.
-			!CASE ("toad")
-			CASE (2)
-				spielFeld(xPosition + 1, yPosition + 0) = .TRUE.
-				spielFeld(xPosition + 2, yPosition + 0) = .TRUE.	
-				spielFeld(xPosition + 3, yPosition + 0) = .TRUE.
-				spielFeld(xPosition + 0, yPosition + 1) = .TRUE.
-				spielFeld(xPosition + 1, yPosition + 1) = .TRUE.
-				spielFeld(xPosition + 2, yPosition + 1) = .TRUE.
-			!CASE ("beacon")
-			CASE (3)
-				spielFeld(xPosition + 0, yPosition + 0) = .TRUE.
-				spielFeld(xPosition + 1, yPosition + 0) = .TRUE.
-				spielFeld(xPosition + 0, yPosition + 1) = .TRUE.
-				spielFeld(xPosition + 3, yPosition + 2) = .TRUE.
-				spielFeld(xPosition + 2, yPosition + 3) = .TRUE.
-				spielFeld(xPosition + 3, yPosition + 3) = .TRUE.
-			!CASE ("glider")
-			CASE (4)
-				spielFeld(xPosition + 0, yPosition + 0) = .TRUE.
-				spielFeld(xPosition + 0, yPosition + 2) = .TRUE.
-				spielFeld(xPosition + 2, yPosition + 1) = .TRUE.
-				spielFeld(xPosition + 1, yPosition + 2) = .TRUE.
-				spielFeld(xPosition + 2, yPosition + 2) = .TRUE.
-			! Default "blinker"
-			CASE DEFAULT
-				spielFeld(xPosition, yPosition) = .TRUE.
-				spielFeld(xPosition + 1, yPosition) = .TRUE.
-				spielFeld(xPosition + 2, yPosition) = .TRUE.
-		END SELECT		
+		!Waste some time.
+		call portable_sleep(0.3)
+		deallocate(characterMatrix)
+	end subroutine
+	
+end module
 
-	END SUBROUTINE createFigures
-
-END MODULE mod_initializeField	
