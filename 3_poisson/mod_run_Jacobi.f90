f55e90c46ba8bcf43dc7c792ec279f2b0a154f60
darklefknight <darklefknight@web.de>
2017-04-30 18:29:47 +0200

    Jakobimatrix funktioniert

--- 3_poisson/mod_run_Jacobi.f90
+++ 3_poisson/mod_run_Jacobi.f90
@@ -2,23 +2,21 @@ MODULE run
 	IMPLICIT NONE
 	CONTAINS
 	
-	subroutine calculate(matrix)
+	subroutine calculate(matrix,checkExit)
 		double precision, allocatable, dimension(:,:), intent(inout) :: matrix !matrix
 		double precision, allocatable, dimension(:,:) :: temp !temporäre Kopie
 		double precision :: star, corr, h !Hilfsvariable
+                integer :: diff, counter !Zähler in Schleifen
+                logical, intent(inout) :: checkExit
 		integer :: i, j !Schleifenvariablen
 		allocate(temp(ubound(matrix, 1), ubound(matrix, 2)))
 		h = 1.0 / (ubound(matrix, 1) - lbound(matrix, 1))
         
 		! Rechnet nicht an den Rändern
-		do i = lbound(matrix, 1)+1, ubound(matrix, 1)-1
-			do j = lbound(matrix, 2)+1, ubound(matrix, 2)-1
+		do j = lbound(matrix, 2)+1, ubound(matrix, 2)-1
+		        do i = lbound(matrix, 1)+1, ubound(matrix, 1)-1
 				! Stern
-				star = (matrix(i,j) * 4)
-				star = star + matrix((i - 1),j)
-				star = star + matrix((i + 1),j)
-				star = star + matrix(i,(j - 1))
-				star = star + matrix(i,(j + 1))
+				star = -matrix(i,j+1)-matrix(i-1,j)+4*matrix(i,j)-matrix(i+1,j)-matrix(i,j-1)
 				! Korrektur
 				corr = matrix(i,j) * h * h - star/4
 				! Übertrag
@@ -38,9 +36,29 @@ MODULE run
 			temp(j,i) = matrix(j,i) ! unten
 		end do
 		
-		! Übertrag
-		matrix = temp
+                diff = 0
+                counter = 0
+                
+                !checken, ob die Lösung schon genau genug ist:
+		do j = lbound(matrix, 2)+1, ubound(matrix, 2)-1
+		        do i = lbound(matrix, 1)+1, ubound(matrix, 1)-1
+                                ! (1) Wenn der neu berechnete Wert weniger als 10e-6 von dem alten abweicht:
+                                if (abs(temp(i,j) -matrix(i,j)) <= 10e-6 ) then
+                                        diff = diff +1
+                                endif
+                                counter = counter +1
+                        enddo
+                enddo
+                
+                !Wenn (1) für alle Werte in der Matrix zutrifft:
+                if (diff == counter) then
+                        checkExit = .True.  !beendet die Schleife in poission.f90
+                endif
 		
+                ! Übertrag
+		matrix = temp   !Selbst wenn die Abbruchbedingung erfüllt ist, so muss der bereits berechnete Wert
+                                !ja trotzdem nicht weggeworfen werden.
+		deallocate(temp)
 	end subroutine
 
 END MODULE run
--- 3_poisson/poisson.f90
+++ 3_poisson/poisson.f90
@@ -5,15 +5,19 @@ PROGRAM Poisson
 	IMPLICIT NONE
 	!
 	double precision, dimension(:,:), allocatable :: matrix ! or whatever fits
-	integer, parameter :: outputSize = 9
+	integer, parameter :: outputSize = 9,loopSize = 40000
 	integer :: i 
-
+        logical :: checkExit =.False.
 	call createMatrix(matrix)
 	call initializeMatrix(matrix)
 	
-	do i = 0, 40000
-		print *, "run ", i
-		call calculate(matrix)
+	do i = 0, loopSize
+		!print *, "run ", i
+		call calculate(matrix,checkExit)
+                if (checkExit) then
+                    write(*,*) 'Abbruchbedingung nach ' , i , 'Durchläufen erfüllt.'
+                    exit
+                endif
 	end do
 	
 	call outputMatrix(matrix, outputSize)
