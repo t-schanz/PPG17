be9a24347f0d09300a095c9ec4464ce7408e80a4
Tobias Machnitzki <machnitzki@wr.informatik.uni-hamburg.de>
2017-05-13 12:38:14 +0200

    parallelisierung funktioniert, jedoch ist die summe noch nicht bei allen gleich.

---
+++ 5A_Scatter/mod_calc.f90
@@ -0,0 +1,23 @@
+MODULE calc
+    implicit none
+    CONTAINS
+    !subroutine computation
+
+    subroutine computation(matrix,rank)
+        integer, dimension(:,:), intent(inout), allocatable :: matrix
+        integer, intent(in) :: rank
+        integer :: i,j
+        integer :: matrix_sum
+
+        do j=lbound(matrix,2), ubound(matrix,2)
+            do i=lbound(matrix,1), ubound(matrix,1)
+                matrix(i,j) = matrix(i,j) *(rank +1)           
+            enddo
+        enddo
+        matrix_sum = sum(matrix)
+        write(*,*) 'Summe von Prozessor ', rank, ' = ', matrix_sum 
+    end subroutine
+
+
+END MODULE
+
