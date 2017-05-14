01c36f335ae632d56f769851322bfc363654d1d9
Tobias Machnitzki <machnitzki@wr.informatik.uni-hamburg.de>
2017-05-14 10:21:46 +0200

    Added 5B Visual. Task part 1 and 2 running

---
+++ 5B_Visual/main.f90
@@ -0,0 +1,50 @@
+program calc_pi
+	use mpi
+
+
+	implicit none
+
+        character(len=80) :: string !Für Afgabenteil 1
+	integer :: ierr, rank, size, tag, status(MPI_STATUS_SIZE)
+	integer, parameter :: master = 0 !Masterprozess festgelegt
+	integer :: i !Schleifenvariable
+        integer :: single_int =5   !Für Aufgabenteil 2
+
+
+	call MPI_INIT(ierr)
+	! Rank und Size in Erfahrung bringen
+	    call MPI_COMM_RANK(MPI_COMM_WORLD,rank,ierr)
+	    call MPI_COMM_SIZE(MPI_COMM_WORLD,size,ierr)
+
+	    if (rank .ne. master) then
+                write(string,"('Hello from',I3)") rank 
+		call MPI_SEND(string,80,MPI_CHARACTER,master,1,MPI_COMM_WORLD,ierr)
+	    else
+		do i = 1, size -1
+			call MPI_RECV(string,80,MPI_CHARACTER,i,1,MPI_COMM_WORLD,status,ierr)
+                        print *, string
+		enddo
+
+	    endif
+
+            call MPI_BARRIER(MPI_COMM_WORLD,ierr)
+
+
+            if (rank .eq. master) then
+                single_int = 6
+            endif
+
+            call MPI_BCAST(single_int,1,MPI_INTEGER,master,MPI_COMM_WORLD,ierr)
+
+            if (rank .ne. master) then
+                write(*,*) single_int
+            endif
+
+            call MPI_BARRIER(MPI_COMM_WORLD,ierr)
+
+
+
+	call MPI_FINALIZE(ierr)
+
+
+end program calc_pi
